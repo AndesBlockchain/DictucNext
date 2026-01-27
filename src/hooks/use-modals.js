59356078fc86c85288cc9c8ae869d42a282e8623
Patricio Lopez <patricio.lopez.cerda@gmail.com>
2026-01-27 11:37:31 -0300

    fix ruta local

--- src/hooks/use-modals.js
+++ src/hooks/use-modals.js
@@ -3,9 +3,7 @@ const useModals = async() => {
   const baseUrl = process.env.STRAPI_API_URL;
   const path = "/api/carruseles";
 
-  //const url = new URL(path, baseUrl);
-
-  const res = await fetch("http://127.0.0.1:1337/api/carruseles");
+  const res = await fetch(baseUrl + path);
 
   if (!res.ok) throw new Error("Failed to fetch carruseles");
 

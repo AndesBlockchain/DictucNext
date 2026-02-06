6dee38bdb71af7eaab65ac183afa23c1b6712b04
Patricio López Cerda <patricio.lopez@andesblockchain.com>
2026-02-06 18:31:33 -0300

    se corrije problema al pasar datos al componente menu secundario (#11)

--- src/components/MenuSecundario.js
+++ src/components/MenuSecundario.js
@@ -1,15 +1,12 @@
 import React from "react";
-import useMenuSecundario from "../hooks/use-menu-secundario";
-import { getFullUrl } from "../helpers/links-helpers";
-import useSiteMetadata from "../hooks/use-site-metadata";
 
-const MenuSecundario = (items) => {
+const MenuSecundario = ({ items, slug }) => {
 
     return (
         <div className="w-full flex justify-center items-center">
             <div className="p-2 w-3/4 flex flex-row justify-center">
-                {items.items.map((item,index)=>(
-                    <div className={`font-medium text-sm pl-4 pr-4 ${item.Link ? 'border-azul-dictuc border-b-2 text-azul-dictuc' : 'border-black border-b-1'}`} key={"menu"+index}>
+                {items && items.map((item, index) => (
+                    <div className={`font-medium text-sm pl-4 pr-4 ${item.Link && item.Link.includes(slug) ? 'border-azul-dictuc border-b-2 text-azul-dictuc' : 'border-black border-b-1'}`} key={"menu" + index}>
                         <a href={item.Link}>{item.Titulo}</a>
                     </div>
                 ))}

af1ab4defea97ca0c93b7ab70ff4caad2892a620
Patricio Lopez <patricio.lopez.cerda@gmail.com>
2026-03-31 13:39:27 -0300

    menu decundairo semibold

--- src/components/MenuSecundario.js
+++ src/components/MenuSecundario.js
@@ -6,7 +6,7 @@ const MenuSecundario = ({ items, slug }) => {
         <div className="w-full flex justify-center items-center">
             <div className="p-2 w-3/4 flex flex-row justify-center">
                 {items && items.map((item, index) => (
-                    <div className={`font-bold text-sm pl-4 pr-4 ${item.Link && item.Link.includes(slug) ? 'border-azul-dictuc border-b-2 text-azul-dictuc' : 'border-black border-b-1'}`} key={"menu" + index}>
+                    <div className={`font-semibold text-sm pl-4 pr-4 ${item.Link && item.Link.includes(slug) ? 'border-azul-dictuc border-b-2 text-azul-dictuc' : 'border-black border-b-1'}`} key={"menu" + index}>
                         <a href={item.Link}>{item.Titulo}</a>
                     </div>
                 ))}

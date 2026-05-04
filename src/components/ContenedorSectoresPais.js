53057bd90a1d7b07c9502b2c5b9d971c357f45b6
Patricio Lopez <patricio.lopez.cerda@gmail.com>
2026-05-03 23:41:55 -0400

    responsividad bloque sectores pais

--- src/components/ContenedorSectoresPais.js
+++ src/components/ContenedorSectoresPais.js
@@ -14,12 +14,12 @@ const ContenedorSectoresPais = async ({itemsPorFila}) => {
   }
 
   return (
-    <div id="items-servicios" className={"container max-w-4xl mx-auto grid grid-cols-" + itemsPorFila + " gap-8 mt-8 pl-2 pr-2 justify-center"}>
+    <div id="items-servicios" className={"container max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-" + itemsPorFila + " gap-8 mt-8 pl-2 pr-2 justify-center"}>
       {sectores.map((item) => {
         const imageUrl = item.Foto?.url;
 
         return (
-          <div key={item.slug} className="flex">
+          <div key={item.slug} className="flex mx-auto">
             <a
               href={"/sectores-pais/" + item.slug}
               className="justify-center w-[280px] h-auto group"
